81dc86c8d702aefdfa5ee172eb5aefc910628be7
kirkby <kirkby@neighbor.com>
2021-02-15 21:24:14 -0700

    progress

--- client/src/index.js
+++ client/src/index.js
@@ -49,7 +49,7 @@ const App = () => {
         path="/item-list/:id"
         render={props => <Item {...props} items={items} setItems={setItems} />}
       />
-      <Route path="/item-form" component={ItemForm} />
+      <Route path="/item-form" component={ItemForm} setItems={setItems} />
       <Route
         path="/update-item/:id"
         render={() => <UpdateForm setItems={setItems} />}
