59501bfb4a27af54c2f52b533b9cf5329806385d
kirkby <kirkby@neighbor.com>
2021-04-12 20:37:12 -0600

    progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -43,6 +43,7 @@ const UpdateForm = props => {
     axios.put(`http://localhost:3333/items/${id}`, item)
       .then(res => {
         console.log('bk: items/put: res: ', res)
+        props.setItems(res.data)
       })
       .catch(err => console.error(`unable to find item by id ${id}: `, err))
   };
